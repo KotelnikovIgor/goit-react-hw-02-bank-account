b8258af5fc36d5719478fb2724caf50461559744
Kotelnikov Igor <digikot1996@gmail.com>
2019-11-25 20:16:18 +0200

    Updates

---
+++ precache-manifest.933b3d792eec707cdaa9dd06c2a5fc55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716f2410acef3efa24b7a217f1c3ebc3",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "f0c33d1a2c0bd16bb21c",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.93123afb.chunk.css"
+  },
+  {
+    "revision": "6656debcce9cc69883f9",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.8492d35c.chunk.js"
+  },
+  {
+    "revision": "f0c33d1a2c0bd16bb21c",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.ef99e6df.chunk.js"
+  },
+  {
+    "revision": "59b69276754240abb686",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.611f8d42.js"
+  }
+]);
